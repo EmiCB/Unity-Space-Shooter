718997edbb53e90cefe5197ec889cec7f4278135
Emi <emicb55@gmail.com>
2015-07-15 17:32:22 -0500

    Space shooter game
    Enitre game

---
+++ Space Shooter/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+	public float speed;
+
+	public GameObject enemyShot;
+	public Transform enemyShotSpawn;
+	public float enemyFireRate;
+	
+	private float nextFire;
+	
+	void Start()
+	{
+		rigidbody.velocity = transform.forward * speed;
+	}
+
+	void Update ()
+	{
+		if (Time.time > nextFire) 
+		{
+			nextFire = Time.time + enemyFireRate;
+			Instantiate (enemyShot, enemyShotSpawn.position, enemyShotSpawn.rotation);
+		}
+	}
+}
